b6779cea7114cb5bc0fb7cda321bf99d936b7903
Pablo Morralla <pmorrall@emeal.nttdata.com>
2026-04-27 12:12:16 +0200

    Implemented insert into supabase

--- src/app/pages/form/form.ts
+++ src/app/pages/form/form.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SupabaseService } from '../../services/supabase-service';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form.html',
   styleUrl: './form.css',
 })
-export class Form {}
+export class Form {
+  title = '';
+  description = '';
+  category = '';
+  priority = 1;
+  email = '';
+
+  constructor(private supabaseService: SupabaseService){}
+
+  async onFormSubmit(){
+    try {
+      const res = await this.supabaseService.createIssue({
+        title: this.title,
+        description: this.description,
+        category: this.category,
+        priority: this.priority,
+        email: this.email
+      });
+      console.log('Issue created!', res);
+    } 
+    catch(error) {
+      console.error('Error while creating issue: ', error);
+    }
+  }
+}
--- src/model/CreateIssueDto.ts
+++ src/model/CreateIssueDto.ts
@@ -2,6 +2,6 @@ export interface CreateIssueDto {
     title: string;
     description: string;
     category: string;
-    priority: 1|2|3|4|5;
+    priority: number;
     email: string;
 }
