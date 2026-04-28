8e2300e169b7f804a00a256e11b1afd3dbe7d292
Pablo Morralla <pmorrall@emeal.nttdata.com>
2026-04-28 12:54:51 +0200

    Fixed fields' max and min length

--- src/app/pages/form/form.ts
+++ src/app/pages/form/form.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { SupabaseService } from '../../services/supabase-service';
 import { maxLength } from '@angular/forms/signals';
@@ -14,13 +14,16 @@ import { RouterLink } from "@angular/router";
 export class Form {
   form: FormGroup;
 
+  successMessage = signal<string|null>(null);
+  errorMessage = signal<string|null>(null);
+
   constructor(
     private supabaseService: SupabaseService,
     private formBuilder: FormBuilder
   ){
     this.form = formBuilder.group({
-      title: new FormControl('', [Validators.required, Validators.maxLength(32)]),
-      description: new FormControl('', [Validators.required, Validators.maxLength(1024)]),
+      title: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(60)]),
+      description: new FormControl('', [Validators.required, Validators.minLength(20), Validators.maxLength(500)]),
       category: new FormControl('', [Validators.required, Validators.maxLength(32)]),
       priority: new FormControl(1, [Validators.required, Validators.min(1), Validators.max(5)]),
       email: new FormControl('', [Validators.required, Validators.email])
@@ -60,10 +63,13 @@ export class Form {
         priority: this.form.get('priority')?.value,
         email: this.form.get('email')?.value
       });
+
       console.log('Issue created!', res);
+      this.successMessage.set('Issue created successfully.');
     } 
     catch(error) {
       console.error('Error while creating issue: ', error);
+      this.successMessage.set('There was an error creating the issue. Try again later.');
     }
   }
 }
